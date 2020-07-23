e80ec0a0efbc8481d8e5d0eab4ecfcb533324217
eimantaspav <63236210+eimantaspav@users.noreply.github.com>
2020-07-23 21:41:43 +0300

    Added go back 1 dir, go back home links, added more comments

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-// GAUNAMAS FAILO TIPO GALUNE KAIP STRINGAS, JEIGU TAI FOLDERIS, GAUNAMA 'DIR'
+// GAUNAMA FAILO TIPO GALUNE KAIP STRINGAS, JEIGU TAI FOLDERIS, DUODAMA 'dir'
 function get_file_extension($file_name)
 {
     if (pathinfo($file_name, PATHINFO_EXTENSION) == '') {
@@ -9,36 +9,6 @@ function get_file_extension($file_name)
         return pathinfo($file_name, PATHINFO_EXTENSION);
     }
 }
-?>
 
-<?php
 
-function go_to()
-{
-    $path = opendir('./' . str_replace('./', './' . $_GET['path'], './'));
-    echo '<table cellpadding="10">'
-        . '<tr class="info">'
-        . '<td>' . 'TYPE' . '</td>'
-        . '<td>' . 'NAME' . '</td>'
-        . '<td>' . 'ACTION' . '</td>'
-        . '</tr>';
-    while (($fName = readdir($path)) !== FALSE) {
-
-        if ($fName != '.' && $fName != '..' && $fName != '.git' && get_file_extension($fName) == 'dir') {
-            echo '<tr>' . '<td>' . get_file_extension($fName) . '</td>'
-                . '<td>' . '<a href="index.php?path=' . $_GET['path'] .  '/' . $fName . '">' . $fName . '</td>'
-                . '<td>' . 'PLACEHOLDER' . '</td>' . '</tr>';
-        }
-        if ($fName != '.' && $fName != '..' && get_file_extension($fName) != 'dir') {
-            echo '<tr>' . '<td>' . get_file_extension($fName) . '</td>'
-                . '<td>' . $fName . '</td>'
-                . '<td>' . 'PLACEHOLDER' . '</td>' . '</tr>';
-        }
-    }
-    echo '</table>';
-    echo '$_GET path ---' . $_GET['path'] . '<br>';
-    echo '$_SERVER[QUERY_STRING] ---' . $_SERVER['QUERY_STRING']. '<br>';
-    echo '$_SERVER[REQUEST_URI]---' . $_SERVER['REQUEST_URI'];
-
-}
 ?>
--- index.php
+++ index.php
@@ -21,8 +21,43 @@
 // FUNKCIJU PAEMIMAS
 require 'functions.php';
 
-// NUSKAITOMA DEFAULT DIREKTORIJA, VISKAS SUDEDAMA Į LENTELĘ
-go_to();
+
+// KELIO KINTAMASIS, KURI PADUODU NUSKAITYTI VELIAU SU READDIR();
+$path = opendir('./' . str_replace('./', './' . $_GET['path'], './'));
+// LENTELES PRADZIA
+echo '<table cellpadding="10">'
+    . '<tr class="info">'
+    . '<td>' . 'TYPE' . '</td>'
+    . '<td>' . 'NAME' . '</td>'
+    . '<td>' . 'ACTION' . '</td>'
+    . '</tr>';
+// NUSKAITOMAs IR I LENTELE SUDEDAMAS PADUOTOS DIREKTORIJOS TURINYS
+while (($fName = readdir($path)) !== FALSE) {
+    // NESPAUSDINAMI ./, ..', .git FOLDERIAI, FOLDERIAMS PAGAL JU EXTENSIONO LYGYBE PRISKIRIAMAS <a> ELEMENTAS
+    if ($fName != '.' && $fName != '..' && $fName != '.git' && get_file_extension($fName) == 'dir') {
+        echo '<tr>' . '<td>' . get_file_extension($fName) . '</td>'
+            . '<td>' . '<a href="index.php?path=' . $_GET['path'] .  '/' . $fName . '">' . $fName . '</td>'
+            . '<td>PLACEHOLDER</td>';
+    }
+    // NESPAUSDINAMI ./, ..', .git FOLDERIAI, FAILAI NEGAUNA <a> ELEMENTO
+    if ($fName != '.' && $fName != '..' && get_file_extension($fName) != 'dir') {
+        echo '<tr>' . '<td>' . get_file_extension($fName) . '</td>'
+            . '<td>' . $fName . '</td>'
+            . '<td>' . 'PLACEHOLDER' . '</td>' . '</tr>';
+    }
+}
+echo '</table>';
+// GRIZIMAS VIENA DIREKTORIJA ATGAL
+echo '<td>' . '<a href="' . dirname($_SERVER['REQUEST_URI'], 1) . '">BACK</td>';
+echo '<br>';
+// GRIZIMAS I PRADINE DIREKTORIJA 
+echo '<td>' . '<a href="./">HOME</td>';
+echo '<br>';
+// SPAUSDINAMA INFORMACIJA
+echo '$_GET path ---' . $_GET['path'] . '<br>';
+echo '$_SERVER[QUERY_STRING] ---' . $_SERVER['QUERY_STRING'] . '<br>';
+echo '$_SERVER[REQUEST_URI]---' . $_SERVER['REQUEST_URI'] . '<br>';
+
 
 
 ?>
