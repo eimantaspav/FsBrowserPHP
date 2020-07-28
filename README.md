3508d4f37651af450ee0a50a9340df764fc66e20
eimantaspav <63236210+eimantaspav@users.noreply.github.com>
2020-07-28 14:52:17 +0300

    Fixed authentication and logout

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Sprint1
+# INSTRUCTIONS:
+
+1. Open visual studio and run a new terminal.
+2. Use the cd command in the terminal to change the working directory to your "Ampps" "www" folder (Example: cd "C:\Program Files\Ampps\www\ (usually the  default installation path).
+3. If you have cloned the project before, please delete the "FsBrowserPHP" folder in the "www" folder.
+4. After making sure that you are in the "www" folder, clone the project with the following command: git clone https://github.com/eimantaspav/FsBrowserPHP.git.
+5. Open your browser enter the following page "http://localhost/FsBrowserPHP/" (without the quatation marks).
+6. To make sure that you are currently viewing the latest version of the project, please press Ctrl+f5 to clear the browser cache.
+7. Now you can login with the username: Eimantas (note the capital E) and password: 123.
+
+FEATURES:
+
+1. Login authentication.
+2. Directory reading, entering, back/home functions.
+3. Directory creation.
+4. File deletion.
+5. Download files. 
+6. Upload "txt", "jpeg", "jpg", "png", "pdf"files up to 2MB.
+7. Logout function.
+
--- index.php
+++ index.php
@@ -1,143 +1,12 @@
 <?php
-session_set_cookie_params(10);
 session_start();
-$loginErrorMsg = '';
-if (isset($_POST['login']) && !empty($_POST['username']) && !empty($_POST['password'])) {
-    if ($_POST['username'] == 'Eimantas' && $_POST['password'] == '123') {
-        $_SESSION['logged_in'] = true;
-        $_SESSION['username'] = 'Eimantas';
-    } else {
-        $loginErrorMsg = 'Wrong username or password';
-    }
-}
-?>
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <link rel="stylesheet" type="text/css" href="FsBrowser.css">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<script>
-    function hi(name) {
-
-        console.log('' + name);
-    }
-</script>
-
-<body>
-</body>
-
-</html>
-
-<?php
-
-// FUNKCIJU PAEMIMAS
-require 'functions.php';
-
-if (!$_SESSION['logged_in'] == true) {
-    echo ('<div class="form"><form class="login" action="" method="POST">');
-    echo ('<input type="text" name="username" placeholder="Eimantas" required autofocus></br>');
-    echo ('<input type="password" name="password" placeholder="123" required><br>');
-    echo ('<button class="login_btn" type="submit" name="login">Login</button>');
-    echo ('</form></div>');
-    echo ('<h1>' . $loginErrorMsg . '<h1>');
-    die();
-}
-
-
-$username = $_SESSION['username'];
-
-echo '<h1>Directory contents:</h1>';
-
-echo '<p class="currentDir">Current directory: ' . $_GET['path'] . '</p>';
-
-// KELIO KINTAMASIS, KURI PADUODU NUSKAITYTI VELIAU SU READDIR();
-$path = opendir('./' . str_replace('./', './' . $_GET['path'], './'));
-// LENTELES PRADZIA
-echo '<table cellpadding="10">'
-    . '<tr class="info">'
-    . '<td>' . 'TYPE' . '</td>'
-    . '<td>' . 'NAME' . '</td>'
-    . '<td>' . 'ACTION' . '</td>'
-    . '</tr>';
-// NUSKAITOMAs IR I LENTELE SUDEDAMAS PADUOTOS DIREKTORIJOS TURINYS
-while (($fName = readdir($path)) !== FALSE) {
-    // NESPAUSDINAMI ./, ..', .git FOLDERIAI, FOLDERIAMS PAGAL JU EXTENSIONO LYGYBE PRISKIRIAMAS <a> ELEMENTAS
-    if ($fName != '.' && $fName != '..' && $fName != '.git' && get_file_extension($fName) == 'dir') {
-        echo '<tr>' . '<td class="extension">' . get_file_extension($fName) . '</td>'
-            . '<td class="fnames">' . '<a href="index.php?path=' . $_GET['path'] .  '/' . $fName . '">' . $fName . '</td>';
-    }
-    // NESPAUSDINAMI ./, ..', .git FOLDERIAI, FAILAI NEGAUNA <a> ELEMENTO
-    if ($fName != '.' && $fName != '..' && $fName != '.git' && get_file_extension($fName) != 'dir') {
-        echo '<tr>' . '<td class="extension">' . get_file_extension($fName) . '</td>'
-            . '<td class="fnames">' . $fName . '</td>'
-            . '<td class="buttonTD"><form method="POST">
-             <input type="hidden" name="del" value=' . str_replace(' ', '&nbsp;', $fName) . '>
-             <input class="buttons" type="submit" value="DELETE">
-            </form>
-            <form   style="display: inline-block" action="" method="post">
-            <input type="hidden" name="download" value=' . str_replace(' ', '&nbsp;', $fName) . '>
-            <input class="buttons" type="submit" value="DOWNLOAD">
-           </form></td>';
-    }
-}
-echo '</table>';
-// NAUJOS DIREKTORIJOS FORMA IR SUKURIMO LOGIKA
-echo '<form class="new_Dir" method="POST">
-<input type="text" name="new_dir" value="">
-<input  class="new_Dir_submit"type="submit" value="Create Directory">
-</form></td>';
-
-if (isset($_POST['new_dir'])) {
-    $pathForNewDir = './' . $_GET['path'] . '/' . $_POST['new_dir'];
-    mkdir($pathForNewDir, 0755, true);
-    header("Refresh:0");
-}
-
-// UPLOAD 
-echo '<br>
-<form action="" method="post" enctype="multipart/form-data">
-<input type="file" name="upload" id="img" style="display:none;"/>
-<button class="uploadBtn1" type="button">
-<label for="img">Choose file</label>
-</button>
-<button class="uploadBtn2" type="submit">Upload file</button>
- </form>
-<br>';
-
-
-
-if (isset($_GET['action']) and $_GET['action'] == 'logout') {
-    session_start();
-    unset($_SESSION['username']);
-    unset($_SESSION['password']);
-    unset($_SESSION['logged_in']);
-}
-
-
-
-
-// GRIZIMAS VIENA DIREKTORIJA ATGAL, MYGTUKAS NEKURIAMAS PRADINEJE DIREKTORIJOJE
-if ($_SERVER['REQUEST_URI'] != '/FsBrowserPHP/' && $_SERVER['REQUEST_URI'] != '/FsBrowserPHP/index.php?path=') {
-    echo '<td>' . '<a class="btn" href="' . dirname($_SERVER['REQUEST_URI'], 1) . '">BACK</a></td>';
-}
-echo '<br>';
-// GRIZIMAS I PRADINE DIREKTORIJA, MYGTUKAS NEKURIAMAS PRADINEJE DIREKTORIJOJE 
-if ($_SERVER['REQUEST_URI'] != '/FsBrowserPHP/' && $_SERVER['REQUEST_URI'] != '/FsBrowserPHP/index.php?path=') {
-    echo '<td>' . '<a class="btn" href="./">HOME</a></td>';
-}
-echo '<br>';
-
-echo ('<div><form action="logout" method="POST">');
-echo ('<button class="logout_btn" type="submit" name="logout">LOGOUT</button>');
-echo ('</form></div>');
-// file download logic
+// Download logic 
 if (isset($_POST['download'])) {
     $dlFilePath = './' . $_GET["path"] . '/' . $_POST['download'];
     $dlFilePathFixed = str_replace("&nbsp;", " ", htmlentities($dlFilePath, null, 'utf-8'));
+    ob_clean();
+    ob_start();
     header('Content-Description: File Transfer');
     header('Content-Type: application/pdf');
     header('Content-Disposition: attachment; filename=' . basename($dlFilePathFixed));
@@ -147,10 +16,11 @@ if (isset($_POST['download'])) {
     header('Pragma: public');
     header('Content-Length: ' . filesize($dlFilePathFixed));
     readfile($dlFilePathFixed);
+    ob_flush();
     exit;
 }
 
-// file upload logic
+// Upload logic 
 if (isset($_FILES['upload'])) {
     $errors = array();
     $file_name = $_FILES['upload']['name'];
@@ -159,7 +29,7 @@ if (isset($_FILES['upload'])) {
     $file_type = $_FILES['upload']['type'];
     $file_ext = strtolower(end(explode('.', $_FILES['upload']['name'])));
 
-    $extensions = array("jpeg", "jpg", "png", "pdf");
+    $extensions = array("txt", "jpeg", "jpg", "png", "pdf");
 
     if (in_array($file_ext, $extensions) === false) {
         $errors[] = "extension not allowed, please choose a JPEG, PNG or PDF file.";
@@ -171,7 +41,6 @@ if (isset($_FILES['upload'])) {
 
     if (empty($errors) == true) {
         move_uploaded_file($file_tmp, './' . $_GET["path"] . '/' . $file_name);
-        //    echo "Success";
     } else {
         echo ($_FILES);
         echo ('<br>');
@@ -179,12 +48,33 @@ if (isset($_FILES['upload'])) {
     }
 }
 
-// JEIGU REIKES ATSISPAUSDINTI INFO
-// echo '$_GET path ---' . $_GET['path'] . '<br>';
-// echo '$_SERVER[QUERY_STRING] ---' . $_SERVER['QUERY_STRING'] . '<br>';
-// echo '$_SERVER[REQUEST_URI]---' . $_SERVER['REQUEST_URI'] . '<br>';
+// Login logic 
+$loginErrorMsg = '';
+if (isset($_POST['login']) && !empty($_POST['username']) && !empty($_POST['password'])) {
+    if ($_POST['username'] == 'Eimantas' && $_POST['password'] == '123') {
+        $_SESSION['logged_in'] = true;
+        $_SESSION['username'] = 'Eimantas';
+    } else {
+        $loginErrorMsg = 'Wrong username or password';
+    }
+}
 
-// FAILO ISTRYNIMO LOGIKA
+// Logout logic 
+if (isset($_POST['logout'])) {
+    session_start();
+    unset($_SESSION['username']);
+    unset($_SESSION['password']);
+    unset($_SESSION['logged_in']);
+    header("Refresh:0");
+}
+
+// New dir logic 
+if (isset($_POST['new_dir'])) {
+    $pathForNewDir = './' . $_GET['path'] . '/' . $_POST['new_dir'];
+    mkdir($pathForNewDir, 0755, true);
+}
+
+// Deletion logic 
 if (isset($_POST['del'])) {
     $pathDelFile = './' . $_GET['path'] . '/' . $_POST['del'];
     $pathDelFileFixed = str_replace("&nbsp;", " ", htmlentities($pathDelFile, null, 'utf-8'));
@@ -197,5 +87,100 @@ if (isset($_POST['del'])) {
 }
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
 
-<!-- JAVA SCRIPT -->
+<head>
+    <link rel="stylesheet" type="text/css" href="FsBrowser.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+
+    // Login form 
+    if (!$_SESSION['logged_in'] == true) {
+        echo '<div class="form">
+        <form class="login" action="" method="POST">
+        <input class="login_input" type="text" name="username" placeholder="Eimantas" required autofocus></br>
+        <input class="login_input" type="password" name="password" placeholder="123" required><br>
+        <button class="login_btn" type="submit" name="login">Login</button>
+        </form></div>
+        <h1>' . $loginErrorMsg . '<h1>';
+        die();
+    }
+
+    // Dirs and btns 
+    echo '<h1>Directory contents:</h1>';
+    echo '<p class="currentDir">Current directory: ' . $_GET['path'] . '</p>';
+    $path = opendir('./' . str_replace('./', './' . $_GET['path'], './'));
+    echo '<table cellpadding="10">'
+        . '<tr class="info">'
+        . '<td>' . 'TYPE' . '</td>'
+        . '<td>' . 'NAME' . '</td>'
+        . '<td>' . 'ACTION' . '</td>'
+        . '</tr>';
+    while (($fName = readdir($path)) !== FALSE) {
+        if ($fName != '.' && $fName != '..' && $fName != '.git' && get_file_extension($fName) == 'dir') {
+            echo '<tr>' . '<td class="extension">' . get_file_extension($fName) . '</td>'
+                . '<td class="fnames">' . '<a href="index.php?path=' . $_GET['path'] .  '/' . $fName . '">' . $fName . '</td>';
+        }
+        if ($fName != '.' && $fName != '..' && $fName != '.git' && get_file_extension($fName) != 'dir') {
+            echo '<tr>' . '<td class="extension">' . get_file_extension($fName) . '</td>'
+                . '<td class="fnames">' . $fName . '</td>'
+                . '<td class="buttonTD"><form method="POST">
+             <input type="hidden" name="del" value=' . str_replace(' ', '&nbsp;', $fName) . '>
+             <input class="buttons" type="submit" value="DELETE">
+            </form>
+            <form   style="display: inline-block" action="" method="post">
+            <input type="hidden" name="download" value="' . str_replace(' ', '&nbsp;', $fName) . '">
+            <input class="buttons" type="submit" value="DOWNLOAD">
+           </form></td>';
+        }
+    }
+    echo '</table>';
+    echo '<form class="new_Dir" method="POST">
+<input type="text" name="new_dir" value="">
+<input  class="new_Dir_submit"type="submit" value="Create Directory">
+</form></td>';
+
+
+    echo '<br>
+<form action="" method="post" enctype="multipart/form-data">
+<input type="file" name="upload" id="img" style="display:none;"/>
+<button class="uploadBtn1" type="button">
+<label for="img">Choose file</label>
+</button>
+<button class="uploadBtn2" type="submit">Upload file</button>
+ </form><br>';
+
+    if ($_SERVER['REQUEST_URI'] != '/FsBrowserPHP/' && $_SERVER['REQUEST_URI'] != '/FsBrowserPHP/index.php?path=') {
+        echo '<td>' . '<a class="btn" href="' . dirname($_SERVER['REQUEST_URI'], 1) . '">BACK</a></td>';
+    }
+    echo '<br>';
+    if ($_SERVER['REQUEST_URI'] != '/FsBrowserPHP/' && $_SERVER['REQUEST_URI'] != '/FsBrowserPHP/index.php?path=') {
+        echo '<td>' . '<a class="btn" href="./">HOME</a></td><br>';
+    }
+
+    echo ('<div><form action="" method="POST">');
+    echo ('<input type="hidden" name="logout">');
+    echo ('<button class="logout_btn" type="submit">Logout</button>');
+    echo ('</form></div>');
+    ?>
+</body>
+
+</html>
+<?php
+
+function get_file_extension($file_name)
+{
+    if (pathinfo($file_name, PATHINFO_EXTENSION) == '') {
+        return "dir";
+    } else {
+        return pathinfo($file_name, PATHINFO_EXTENSION);
+    }
+}
+
+?>
